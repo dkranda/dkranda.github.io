e27a9f9da571ef488939a914e146e72cb1b5dcf5
Dan Kranda <dkranda@gmail.com>
2019-01-04 18:45:11 -0600

    Adjust dice positioning

--- shut-the-box/tile.js
+++ shut-the-box/tile.js
@@ -1,25 +1,38 @@
-class Tile
-{
-  constructor(tileNumber)
-  {
+class Tile {
+  constructor(tileNumber) {
     this.tileNumber = tileNumber;
     this.isDown = false;
   }
 
-  dropTile()
-  {
+  dropTile() {
     this.isDown = true;
   }
 }
 
-class Die
-{
-  constructor(number,wt,ht)
-  {
+class Die {
+  constructor(number, wt, ht, existingDice) {
     this.number = number;
     this.xpos = Math.floor(Math.random() * wt * 0.80 + (wt * 0.10));
-    this.ypos = Math.floor((Math.random() * ht * 0.50) + (ht * 0.40))
+    this.ypos = Math.floor((Math.random() * ht * 0.40) + (ht / 3) + (ht * 0.10))
     this.w = (wt * 0.08);
     this.r = (wt * 0.01);
+
+    // avoid overlapping dice
+    if (existingDice != null) {
+      for (var i = 0; i < existingDice.length; i++) {
+        var xdiff = (this.xpos - existingDice[i].xpos);
+        var ydiff = (this.ypos - existingDice[i].ypos);
+
+        if ((Math.abs(xdiff) < this.w) && (Math.abs(ydiff) < this.w))
+        {
+          if (Math.abs(xdiff) < this.w) {
+            this.xpos += (Math.sign(xdiff) * this.w);
+          }
+          if (Math.abs(ydiff) < this.w) {
+            this.ypos += (Math.sign(ydiff) * this.w);
+          }
+        }
+      }
+    }
   }
 }
